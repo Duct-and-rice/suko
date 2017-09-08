27f43c540c3ee45cba1b228a05cb8e26715b6fbe
duct@duct <duct@duct>
2017-09-08 23:34:06 +0900

    eslint & babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "standard"
+    "extends": ["standard"],
+    "parser": "babel-eslint",
+    "plugins": ["import"],
+    "rules": {
+        "no-var": 2,
+        "import/no-commonjs": 2
+
+    }
 };
